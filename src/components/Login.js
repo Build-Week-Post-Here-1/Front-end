af5a713b8b7fda233f84ea5c10605213f227e1e6
Daniel Mattox <dmattox10@gmail.com>
2020-02-03 14:44:51 -0500

    Fixed Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { useInput } from '../hooks/useInput'
+import { apiUrl } from '../config'
 
+// This file wasn't so bad, aside from zero wiring
 const Login = props => {
+    const append = ''
+    const history = useHistory()
+    const { value:user, bind:bindUser } = useInput('')
+    const { value:pass, bind:bindPass } = useInput('')
+    const [values, updateValues] = useState({
+      user: '',
+      pass: ''
+    })
+
+    useEffect(() => {
+      updateValues({
+        user: user,
+        pass: pass
+      })
+    }, [user, pass])
+
+    const handleSubmit = e => {
+      e.preventDefault()
+      axios
+        .get(`${apiUrl}${append}`, values)
+        .then(res => {
+          console.log(res.data)
+          // We'll store the token here
+          history.push('/')
+        })
+        .catch(err => console.log(err))
+
+    }
     return (
         <Form>
         <FormGroup>
-          <Label for="username">User Name</Label>
-          <Input type="name" name="name" id="username" placeholder="Insert User Name" />
+          <Label for="user">User Name</Label>
+          <Input type="text" {...bindUser} />
         </FormGroup>
         <FormGroup>
           <Label for="password">Password</Label>
-          <Input type="password" name="password" id="redditpw" placeholder="Type your password here" />
+          <Input type="password" {...bindPass} />
         </FormGroup>
         <Button>Log In</Button>
         </Form>
