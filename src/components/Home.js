f708e84f08f270ae41fd0c2b4cb78805fa2556d0
Daniel Mattox <dmattox10@gmail.com>
2020-02-03 17:17:29 -0500

    Moving machines

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Container, Jumbotron, Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { useInput } from '../hooks/useInput'
 
 const Home = props => {
+
+    const [search, updateSearch] = useState('')
+    
+    useEffect(() => {
+        updateSearch(search)
+    }, [search])
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
     return (
-        <>
-            <h2>Home, Search will go here</h2>
-        </>
+        <Container>
+            <div className='spacer'></div>
+            <Jumbotron>
+                <Form onSubmit={ handleSubmit }>
+                    <FormGroup>
+                        <Label for='search'>Search by entering the full text of your post</Label>
+                        <Input type='text' name='search' id='search' value={ search } />
+                    </FormGroup>
+                </Form>
+            </Jumbotron>
+        </Container>
     )
 }
 
