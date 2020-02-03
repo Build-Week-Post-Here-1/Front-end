5bcfd78433ba73103738ffd467b34c6db63e7c6c
Daniel Mattox <dmattox10@gmail.com>
2020-02-03 18:34:09 -0500

    Added post component, added search to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import { Container, Jumbotron, Form, FormGroup, Label, Input, Button } from 'reactstrap'
 import { useInput } from '../hooks/useInput'
+import { searchUrl } from '../config'
+import axios from 'axios'
+
+import Post from './Post'
 
 const Home = props => {
 
-    const [search, updateSearch] = useState('')
+    const [searchTerm, updateSearchTerm] = useState('')
+    const [posts, updatePosts] = useState([])
+
+    const { value:search, bind:bindSearch } = useInput('')
     
     useEffect(() => {
-        updateSearch(search)
+        updateSearchTerm(search)
     }, [search])
+
     const handleSubmit = e => {
         e.preventDefault()
+        axios
+        .get(searchUrl, searchTerm)
+        .then(res => {
+            updatePosts(res.data)
+        })
+        .catch(err => console.log(err))
     }
     return (
         <Container>
@@ -19,9 +33,11 @@ const Home = props => {
                 <Form onSubmit={ handleSubmit }>
                     <FormGroup>
                         <Label for='search'>Search by entering the full text of your post</Label>
-                        <Input type='text' name='search' id='search' value={ search } />
+                        <Input type='text' {...bindSearch} />
                     </FormGroup>
+                    <Button type='submit'>Search</Button>
                 </Form>
+                { posts.map(post => <Post post={ post } />) }
             </Jumbotron>
         </Container>
     )
