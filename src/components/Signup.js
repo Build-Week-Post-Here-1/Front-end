7d7373174da31c572e9e09ce7f5e9ee5cc84e3b6
fluxlog <fluxlog@hotmail.com>
2020-02-04 09:38:27 -0800

    Added some basic form validations

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -35,15 +35,16 @@ const Signup = props => {
 
     }
 
+
     return (
         <Form onSubmit={ handleSubmit }>
         <FormGroup>
           <Label for='user'>User Name</Label>
-          <Input type='text' {...bindUser}/>
+          <Input type='text' minLength='3' maxLength='20' required='true' {...bindUser}/>
         </FormGroup>
         <FormGroup>
           <Label for='pass'>Password</Label>
-          <Input type='password' {...bindPass} />
+          <Input type='password' minLength='4' required='true' {...bindPass} />
         </FormGroup>
         <Button>Submit</Button>
       </Form>
@@ -52,57 +53,3 @@ const Signup = props => {
 
 
 export default Signup
-
-// Added by Gunnar without wiring or state
-
-/*
-        <FormGroup>
-          <Label for="Select">I am a:</Label>
-          <Input type="select" name="select" id="exampleSelect">
-            <option>JavaScript Developer</option>
-            <option>Data Scientist</option>
-            <option>UI/UX Designer</option>
-            <option>HTML/CSS connoisseur</option>
-            <option>Full Stack Web Developer</option>
-          </Input>
-        </FormGroup>
-
-        <FormGroup>
-          <Label for="opinionText">Please express your displeasure of this form here:</Label>
-          <Input type="textarea" name="text" id="opinionText" />
-        </FormGroup>
-
-        <FormGroup tag="fieldset">
-          <legend>My favorite food is:</legend>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" />{' '}
-              Pizza
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" />{' '}
-              Hamburger
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" />{' '}
-              Steak
-            </Label>
-          </FormGroup>
-          <FormGroup check disabled>
-            <Label check>
-              <Input type="radio" name="I THINK NOT" disabled />{' '}
-              Some other food
-            </Label>
-          </FormGroup>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" />{' '}
-            I Accept the Terms and Conditions
-          </Label>
-        </FormGroup>
-        */
